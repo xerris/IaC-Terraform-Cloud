246f9708c1694686647e858bb6e0a58f0ce7e1ed
Greg <greg@xerris.com>
2020-11-01 07:34:14 -0700

    removed the .idea folder

---
+++ main.tf
@@ -0,0 +1,6 @@
+
+
+
+resource "aws_s3_bucket" "mys3" {
+  bucket = "xerris-terraform-s3"
+}
--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "aws" {  
-  region: "us-west-2"
-}
-
-resource "aws_s3_bucket" "mys3" {
-  bucket = "xerris-terraform-s3"
+  region = "us-west-2"
 }
