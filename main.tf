417dbe91cadd0e9e5ab9e4df5c0fc1e4db48ba5f
Greg <greg@xerris.com>
2020-11-01 14:19:36 -0700

    created the DynamoDB table

--- main.tf
+++ main.tf
@@ -1,2 +1,14 @@
 
 
+resource "aws_dynamodb_table" "tfc_example_table" {
+  name = "${var.db_table_name}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
 variable "aws_region" {}
+variable "db_table_name" {}
+variable "db_read_capacity" {}
+variable "db_write_capacity" {}
