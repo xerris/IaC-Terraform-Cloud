56b2954b29700def5e6f1daac5f5fb973598831a
Greg <greg@xerris.com>
2020-11-01 14:29:06 -0700

    removed removal policy to table

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_dynamodb_table" "tfc_example_table" {
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
   hash_key       = "Id"
-  removal_policy = "DESTROY"
 
   attribute {
     name = "Id"
