bdec32709758c998b018e7dcd585e3d3763c589b
Greg <greg@xerris.com>
2020-11-01 13:49:04 -0700

    cleaned up unused vars

--- provider.tf
+++ provider.tf
@@ -1,11 +1,4 @@
-provider "aws" {  
-  region = "us-west-2"
-}
-
-locals {
-  common_variables = {
-    AWS_ACCESS_KEY_ID = var.aws_access_key_id
-    AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
-    region = var.region
-  }
+provider "aws" {
+  version = "2.33.0"
+  region = var.aws_region
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,1 @@
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
-variable "region" {}
+variable "aws_region" {}
